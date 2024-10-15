383fd7e20e4562fe9fb458cbdfad0b73e40f222e
Vamabam <vincentyounes04@gmail.com>
2024-10-15 22:41:54 +1100

    Cleaned up Comments, debugged edge cases

--- folder/get_folder.go
+++ folder/get_folder.go
@@ -25,44 +25,47 @@ func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
 
 }
 
-// Edited the function call from original to allow for error throwing
+// - Inputs: OrgID and name of folder
+// - Returns: Array of Child Folders and Errors
+// - Gets all the child folders of the given folder within Org
 func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
 	// Get all paths from org
 	allFolders := f.GetFoldersByOrgID(orgID)
 
 	// Check if Invalid Org
 	if len(allFolders) < 1 {
-		return []Folder{}, errors.New("invalid orgid")
+		return []Folder{}, errors.New("OrgID does not exist")
 	}
 
 	children := []Folder{}
 	for _, Folder := range allFolders {
 		// Split paths by .
 		splitPaths := strings.Split(Folder.Paths, ".")
-		// Check for invalid path through length of splitPaths
 
-		// Check through split paths if name exists
-		// Only len - 1 to ensure only children added
-		for i := 0; i < len(splitPaths); i++ {
+		// Check if name exists in split paths
+		// len(splitPaths) - 1 as final folder in path has no children
+		for i := 0; i < len(splitPaths)-1; i++ {
 			// If name is found add whole Folder struct to children
 			if splitPaths[i] == name && len(splitPaths) > 1 {
 				children = append(children, Folder)
 			}
 		}
-
 	}
-	// If file does not exist in this org then check if in other orgs
+
+	// If file does not exist in this org then check other orgs
 	if len(children) < 1 {
 		folders := f.folders
 		for _, f := range folders {
-			if f.Name == name {
+			if f.Name == name && f.OrgId != orgID {
 				return []Folder{},
 					errors.New("Folder does not exist in the specified organization")
+			} else if f.Name == name {
+				// Has no children
+				return []Folder{}, nil
 			}
 		}
 		// File isnt in the system at all
-		return []Folder{},
-			errors.New("Folder doesn't Exist")
+		return []Folder{}, errors.New("Folder doesn't exist")
 	} else {
 		return children, nil
 	}
